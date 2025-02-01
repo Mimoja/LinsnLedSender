1a0874a7bf0eca4cc4a521c54d94ff762eca1045
Mimoja <git@mimoja.de>
2025-02-01 21:50:11 +0100

    add dragon
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/main.rs
+++ src/main.rs
@@ -91,41 +91,8 @@ fn main() {
     //     thread::sleep(Duration::from_millis(0));
     // }
 
-    // let mut panel = Panel::new(192, 192, false, false);
-
-    // let dragon_imgs = load_image_directory("./dragon");
-    // let mut dragon1 = AnimatedSprite::new(dragon_imgs.clone(), 2.5, sprite::LoopMode::PingPong, 2.0);
-    // let mut dragon2 = AnimatedSprite::new(dragon_imgs.clone(), 2.5, sprite::LoopMode::PingPong, 2.0);
-    // let mut dragon3 = AnimatedSprite::new(dragon_imgs.clone(), 2.5, sprite::LoopMode::PingPong, 2.0);
-    // let mut dragon4 = AnimatedSprite::new(dragon_imgs, 2.5, sprite::LoopMode::PingPong, 2.0);
-
-    // let mut time: f32 = 0.0;
-    // let mut rng = rand::thread_rng();
-    // let d1_x = rng.gen_range(0..panel.width) as i32;
-    // let d1_y = rng.gen_range(0..panel.height) as i32;
-    // let d2_x = rng.gen_range(0..panel.width)as i32;
-    // let d2_y = rng.gen_range(0..panel.height)as i32;
-    // let d3_x = rng.gen_range(0..panel.width)as i32;
-    // let d3_y = rng.gen_range(0..panel.height)as i32;
-    // let d4_x = rng.gen_range(0..panel.width)as i32;
-    // let d4_y = rng.gen_range(0..panel.height)as i32;
-
-    // loop {
-    //         time += 0.02;
-    //         panel.clear();
-
-    //         dragon1.draw(&mut panel, d1_x, d1_y);
-    //         dragon2.draw(&mut panel, d2_x + (time.sin() * 64.0) as i32, d2_y);
-    //         dragon3.draw(&mut panel, d3_x, d3_y + ((time).cos() * 32.0) as i32);
-    //         dragon4.set_scale(3.0+ ((time).cos() * 3.0));
-    //         dragon4.draw(&mut panel, d4_x, d4_y);
-
-    //         panel.send(sender.clone(), dst_mac);
-    //         thread::sleep(Duration::from_millis(0));
-    //     }
-
     let mut panel = Panel::new(192, 192, false, false);
-    
+
     // Train
     let train_imgs = load_image_directory("./assets/train");
     let mut train = AnimatedSprite::new(train_imgs.clone(), 1.0, sprite::LoopMode::PingPong, 2.0);
@@ -137,39 +104,88 @@ fn main() {
     let grass: DynamicImage = image::open("assets/grass.png").unwrap();
     let backgrounds = load_image_directory("assets/background");
 
-    // Sky 
+    // Sky
     let skys = load_image_directory("assets/sky");
+
+    // Dragon
+    let dragon_imgs = load_image_directory("assets/dragon");
+    let mut dragon = AnimatedSprite::new(dragon_imgs, 2.5, sprite::LoopMode::PingPong, 2.0);
 
     let before = Instant::now();
     loop {
-        let ts = Instant::now()-before;
+        let ts = Instant::now() - before;
         panel.clear();
 
         for i in 0..6 {
-            panel.draw_image((i * 32) , 192-32, &grass, 2.0, false, false);
+            panel.draw_image((i * 32), 192 - 32, &grass, 2.0, false, false);
         }
 
         for i in 0..4 {
-           train.draw(&mut panel, 64 * i, 192-32);
+            train.draw(&mut panel, 64 * i, 192 - 32);
         }
 
-        let track_offset = ((ts.as_millis() / 200) % 32 )as i32;
+        let track_offset = ((ts.as_millis() / 200) % 32) as i32;
         for i in 0..8 {
-            panel.draw_image((i * 32) - track_offset, 192-32, &tracks, 2.0, false, false);
-        }
-    
-        let background_offset = ((ts.as_millis() / 500) % 192 )as i32;
-        for i in 0usize..16 {
-            panel.draw_image((i as i32 * 32) - background_offset, 192-64, &backgrounds[i % backgrounds.len()], 2.0, false, false);
+            panel.draw_image(
+                (i * 32) - track_offset,
+                192 - 32,
+                &tracks,
+                2.0,
+                false,
+                false,
+            );
         }
 
-        let sky_offset = ((ts.as_millis() / 500) % 192 )as i32;
+        let background_offset = ((ts.as_millis() / 500) % 192) as i32;
         for i in 0usize..16 {
-            panel.draw_image((i as i32 * 32) - sky_offset, 96, &skys[i % skys.len()], 2.0, false, false);
-            panel.draw_image((i as i32 * 32) - sky_offset, 64, &skys[(i + 6) % skys.len()], 2.0, false, false);
-            panel.draw_image((i as i32 * 32) - sky_offset, 32, &skys[(i + 8) % skys.len()], 2.0, false, false);
-            panel.draw_image((i as i32 * 32) - sky_offset, 0, &skys[(i + 11) % skys.len()], 2.0, false, false);
+            panel.draw_image(
+                (i as i32 * 32) - background_offset,
+                192 - 64,
+                &backgrounds[i % backgrounds.len()],
+                2.0,
+                false,
+                false,
+            );
         }
+
+        let sky_offset = ((ts.as_millis() / 500) % 192) as i32;
+        for i in 0usize..16 {
+            panel.draw_image(
+                (i as i32 * 32) - sky_offset,
+                96,
+                &skys[i % skys.len()],
+                2.0,
+                false,
+                false,
+            );
+            panel.draw_image(
+                (i as i32 * 32) - sky_offset,
+                64,
+                &skys[(i + 6) % skys.len()],
+                2.0,
+                false,
+                false,
+            );
+            panel.draw_image(
+                (i as i32 * 32) - sky_offset,
+                32,
+                &skys[(i + 8) % skys.len()],
+                2.0,
+                false,
+                false,
+            );
+            panel.draw_image(
+                (i as i32 * 32) - sky_offset,
+                0,
+                &skys[(i + 11) % skys.len()],
+                2.0,
+                false,
+                false,
+            );
+        }
+
+        let dragon_offset = ((ts.as_millis() / 100) % 800) as i32;
+        dragon.draw(&mut panel, dragon_offset - 64, 16);
 
         panel.send(sender.clone(), dst_mac);
     }
